0e385be93f93d64644a2bf70db8e0962c5dde895
Mayuriyupe23 <mayuriyupe23@navgurukul.org>
2024-10-22 14:57:56 +0530

    refactor:better shift management

--- appointments_app/appointments_app/doctype/appointment/appointment.js
+++ appointments_app/appointments_app/doctype/appointment/appointment.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2024, Mayuri Tupe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Appointment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Appointment", {
+	refresh(frm) {
+        frm.set_query('shift',(doc)=>{
+            return {
+                filters: {
+                    "clinic": doc.clinic
+                }
+            }
+        })
 
-// 	},
-// });
+	},
+});
