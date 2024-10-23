0d0f81d2e0a5dc7fbdc88406069cce82e22771ef
Mayuriyupe23 <mayuriyupe23@navgurukul.org>
2024-10-23 17:49:39 +0530

    Changes Made for queue numer

--- appointments_app/appointments_app/doctype/appointment/test_appointment.py
+++ appointments_app/appointments_app/doctype/appointment/test_appointment.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, Mayuri Tupe and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestAppointment(FrappeTestCase):
-	pass
+	def test_add_to_appointment__queue(self):
+		doctor = frappe.get_doc({"doctype": "Docter", "name", "Test Doctor"}).insert()
+		clinic = frappe.get_doc({"doctype": "Clinic","name": "Test Clinic", "doctor":doctor.name}).insert()
+
+		day = '10-10-2023'
+		add_to_appointment_queue(clinic, day, shift, appointment)
---
+++ appointments_app/appointments_app/doctype/appointment_queue/test_appointment_queue.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Mayuri Tupe and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAppointmentQueue(FrappeTestCase):
+	pass
